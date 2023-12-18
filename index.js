4acd87ecf2e3a4715d870d2c27873c6d286b2ce2
Kelvin Lin <Kelvinlinnaeus@gmail.com>
2023-12-18 02:22:52 +0000

    Arrays

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  {id: 1, name: 'Course 1'},
+  {id: 2, name: 'Course 2'},
+  {id: 3, name: 'Course 3'}
+]
+
 app.get('/', (req, res) => {
   res.send("Hello Kelvin!");
 })
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3])
+  res.send(courses)
 })
 
 
-// app.get('/api/courses/:id', (req, res) => {
-//   res.send(req.params.id);
-// })
-
-app.get('/api/posts/:year/:month', (req, res) => {
-  res.send(req.query)
-  // http://localhost:3000/api/posts/2018/1?sortBy=name
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if(!course) res.status(404).send("Course not found")
+  res.send(course)
 })
+
 
 const port = process.env.PORT || 3000
 app.listen(3000, () => {
