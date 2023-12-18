c38b06bb8022723d1278480f24c1e4573ac0f76e
Kelvin Lin <Kelvinlinnaeus@gmail.com>
2023-12-18 02:28:45 +0000

    Handling POST

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   {id: 1, name: 'Course 1'},
   {id: 2, name: 'Course 2'},
@@ -15,6 +17,14 @@ app.get('/api/courses', (req, res) => {
   res.send(courses)
 })
 
+app.post('/api/courses', (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course)
+  res.send(course)
+})
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
