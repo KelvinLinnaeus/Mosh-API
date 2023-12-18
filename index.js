964f667e967e2f721fef052c2fde939c92a1e90b
Kelvin Lin <Kelvinlinnaeus@gmail.com>
2023-12-18 02:03:34 +0000

    Route Parameters

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.get('/api/courses', (req, res) => {
 })
 
 
+// app.get('/api/courses/:id', (req, res) => {
+//   res.send(req.params.id);
+// })
+
+app.get('/api/posts/:year/:month', (req, res) => {
+  res.send(req.query)
+  // http://localhost:3000/api/posts/2018/1?sortBy=name
+})
+
 const port = process.env.PORT || 3000
 app.listen(3000, () => {
   console.log(`listening on ${port}`);
